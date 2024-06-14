a8d4458be498ca6e9e7c64a9f9575f74b62e5e58
haohao <1399182243@qq.com>
2024-06-14 13:11:28 +0800

    update video urls

--- generate_video_urls.py
+++ generate_video_urls.py
@@ -11,7 +11,7 @@ for item in bvids_list[:10]:
         id = item['id']
         # print(f' {tag} 标签下的视频')
         for bvid in item['bvid_list']:
-            video_url = f'http://47.94.151.36/{bvid}.mpd'
+            video_url = f'\'http://123.57.76.186/{bvid}.mpd\','
             video_urls.append(video_url)
 
 # 去重
@@ -19,6 +19,11 @@ video_urls = list(set(video_urls))
 
 # 存储到txt
 with open('video_urls.txt', 'w') as f:
+    count = 0
     for url in video_urls:
         f.write(url + '\n')
+        count += 1
+        separator = '--- End of batch ' + str(count / 10) + ' ---\n'
+        if count % 10 == 0:
+            f.write(separator + '\n')
             
