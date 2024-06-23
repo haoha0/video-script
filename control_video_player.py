665d304b9ddd6058101389ee45e2e6c938b6d99b
haohao <1399182243@qq.com>
2024-06-23 11:30:01 +0800

    add video control and log process script

---
+++ control_video_player.py
@@ -0,0 +1,103 @@
+import traceback
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import random
+import os
+
+def load_and_play_video(driver, url, wait):
+    """加载并播放视频，然后在随机时间后暂停。"""
+    input_box = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "form-control")))
+    input_box.clear()
+    input_box.send_keys(url)
+    print(f"URL {url} loaded into input box.")
+
+    # 点击加载并播放视频
+    load_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > div.container > div:nth-child(2) > div.input-group > span > button.btn.btn-primary")))
+    load_button.click()
+    print("Load button clicked.")
+    time.sleep(1) # TODO 这里需要考虑修改等待逻辑
+
+    play_button = wait.until(EC.element_to_be_clickable((By.ID, "iconPlayPause")))
+    play_button.click()
+    print("Play button clicked.")
+
+    # 随机等待时间，播放视频
+    wait_time = random.randint(5, 11)
+    print(f"Playing video for {wait_time - 1} seconds.")
+    time.sleep(wait_time)
+
+    # 暂停视频
+    play_button.click()
+    print("Video paused.")
+
+def save_logs_to_file(driver, folder_name="original_logs", filename="browser_logs.txt"):
+    """获取浏览器日志并保存到文件。"""
+    logs = driver.get_log('browser')
+    # 创建文件夹
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    filepath = os.path.join(folder_name, filename)
+    with open(filepath, "a") as file:
+        for log in logs:
+            # file.write(f"{log['timestamp']} - {log['level']} - {log['message']}\n")
+            file.write(f"{log['timestamp']} - {log['message']}\n")
+    print(f"Logs saved to {filepath}.")
+
+# 主程序
+options = webdriver.EdgeOptions()
+# options.use_chromium = True
+options.set_capability('ms:loggingPrefs', {'browser': 'ALL'})
+options.add_experimental_option("detach", True)
+driver = webdriver.Edge(options=options)
+# TODO:使用指定路径的driver
+# driver_path = '/Users/liuwenhao/Downloads/edgedriver_mac64_m1/msedgedriver'
+# driver = webdriver.Edge(service=webdriver.EdgeService(driver_path), options=options)
+
+driver.get('http://localhost:3000/samples/dash-if-reference-player/index.html')
+
+# 视频URL列表
+video_urls = [
+    'http://123.57.76.186/BV1JP411D76R.mpd',
+    'http://123.57.76.186/BV1Wf4y1n7Pc.mpd',
+    'http://123.57.76.186/BV1CM4m1k7bs.mpd',
+    'http://123.57.76.186/BV1tc411M7JF.mpd',
+    'http://123.57.76.186/BV1pn4y197hv.mpd',
+    'http://123.57.76.186/BV16b421e7L1.mpd',
+    'http://123.57.76.186/BV1UB4y1N7zB.mpd',
+    'http://123.57.76.186/BV1At421t7z4.mpd',
+    'http://123.57.76.186/BV1te4y1E7K5.mpd',
+    'http://123.57.76.186/BV1VG411B7RP.mpd'
+]
+
+# test
+video_urls = [
+    'https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd',
+    'https://dash.akamaized.net/digitalprimates/fraunhofer/480p_video/heaac_2_0_with_video/Sintel/sintel_480p_heaac2_0.mpd'
+]
+
+wait = WebDriverWait(driver, 10)
+try:
+    # test: 一次测试多个视频
+    for i in range(1, 3):   # TODO
+        # 选择前i个视频进行测试
+        selected_videos = video_urls[:i]
+        # selected_videos = random.sample(video_urls, min(i, len(video_urls)))  # 选择min(i, len(video_urls))个视频，防止超出列表长度
+        print(f"Test {i}: Testing with {len(selected_videos)} videos.")
+        for url in selected_videos:
+            load_and_play_video(driver, url, wait)
+            # time.sleep(2)  # 如果需要在视频间有特定的间隔
+
+        folder_name = "original_logs"
+        filename = "log_" + time.strftime("%Y%m%d-%H%M") + f"_test_{i}.txt"
+        save_logs_to_file(driver, folder_name, filename)
+        time.sleep(5)  # 等待一段时间开始下一轮测试
+
+except Exception as e:
+    print("An error occurred:")
+    traceback.print_exc()
+
+driver.quit()
---
+++ process_logs.py
@@ -0,0 +1,48 @@
+import sys
+import re
+import os
+
+# 确保命令行参数正确
+if len(sys.argv) != 2:
+    print("请提供正确的文件名作为命令行参数！")
+    sys.exit(1)
+
+# 获取文件名
+filepath = sys.argv[1]
+filename = os.path.basename(filepath)
+
+# 创建新文件名和目标标签列表
+new_folder_name = "filtered_logs"
+new_filename = f"{filename.split('.')[0]}_filtered.txt"
+new_filepath = os.path.join(new_folder_name, new_filename)
+# target_labels = ['[PlaybackController]', '[video]', '[haohao]', '[MediaPlayer]']    # TODO
+target_labels = ['[PlaybackController]', '[BufferController][video]', '[haohao]', '[MediaPlayer]']
+
+# 编译正则表达式，用于匹配第一个双引号及其后的内容
+pattern = re.compile(r'"(.*?)"')
+
+# 打开原始文件和新文件
+try:
+    # 创建文件夹
+    if not os.path.exists(new_folder_name):
+        os.makedirs(new_folder_name)
+    
+    with open(filepath, 'r') as file, open(new_filepath, 'w') as new_file:
+        manual_found = False  # 标记是否找到第一个包含 "Manual" 的行
+        for line in file:
+            if "Manual" in line and not manual_found:
+                manual_found = True
+            
+            if manual_found:
+                # 判断行中是否含有目标标签
+                if any(label in line for label in target_labels):
+                    # 使用正则表达式进行匹配
+                    match = pattern.search(line)
+                    if match:
+                        content = match.group(1)
+                        new_file.write(content + '\n')
+except FileNotFoundError:
+    print(f"文件 '{filename}' 不存在！")
+    sys.exit(1)
+
+print(f"筛选结果已写入文件 {new_filepath}")
