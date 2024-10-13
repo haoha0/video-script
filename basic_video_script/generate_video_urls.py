c173621105df91b88e4edc4c877db6bcf537240a
haoha0 <1399182243@qq.com>
2024-10-13 20:25:03 +0800

    filter videos and add a new video player script

--- basic_video_script/generate_video_urls.py
+++ basic_video_script/generate_video_urls.py
@@ -1,22 +1,32 @@
 import json
 
 # 读取json文件
-with open('video_bivids_filtered.json', 'r', encoding='utf-8') as file:
+with open('../basic_video_infos/video_bivids_filtered.json', 'r', encoding='utf-8') as file:
     bvids_list = json.load(file)
 
+# print(len(bvids_list))
 video_urls = []
 
 # 手动筛选出垃圾视频，竖屏，劣质内容等
 waste_videos = ['BV1h7421d72b', 'BV1Mm421K7Ph', 'BV1Lr421s7fF', 'BV1Mr421c7og', 'BV1Mw4m1m7TE',
                 'BV1pn4y197hv', 'BV1UB4y1N7zB', 'BV1VG411B7RP', 'BV1vW421P74D', 'BV1Wx4y1e7h8',
-                'BV1YT421v7Pi']
+                'BV1YT421v7Pi', 'BV1mT421Q7Zs', 'BV1j7421o7wd', 'BV1Aa4y1a7mW', 'BV1dH4y1u7Ga']
+
+less_2min = ['BV1EY411d77Z', 'BV1Mr421p7W4', 'BV1rj411J7sN', 'BV11N4y197Ng', 'BV14m421N7Q5',
+             'BV1Dr421u7fQ', 'BV1Lr421s7fF', 'BV1Mr421c7og', 'BV1Np4y157Gs',
+             'BV1ks411W7r5', 'BV1mm421N7b1', 'BV1rm42157NK',
+             'BV1qv4y1p7YN', 'BV1w84y1t7ah',
+             'BV1a24y1j74Z', 'BV1BV4y1T769',
+             'BV1ai421D7yu', 'BV1Br421w7Pz', 'BV1pz421a7oC', 'BV1zi421S7QJ',
+             'BV1Sp4y1j78N', 'BV1te4y1E7K5', 'BV16Y4y13724',
+             'BV1Am421K7LM', 'BV1Sf421Q7vU', 'BV1Vm421T7zL', 'BV1YT421v7Pi']
 
 for item in bvids_list[:10]:
         tag = item['tag_name']
         id = item['id']
         # print(f' {tag} 标签下的视频')
         for bvid in item['bvid_list']:
-            if bvid in waste_videos:    # 跳过垃圾视频
+            if bvid in waste_videos or bvid in less_2min:    # 跳过垃圾视频
                 continue
             # print(bvid)
             video_url = f'f\'http://{{server_ip}}/{bvid}.mpd\','
--- inorder_control_video_player_test.py
+++ inorder_control_video_player_test.py
@@ -8,7 +8,7 @@ import os
 import re
 import subprocess
 
-def load_and_play_video(driver, url, playtime, wait):
+def load_and_play_video(driver, url, playtime, wait, tshark_command):
     """加载并播放视频，然后在随机时间后暂停。"""
     input_box = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "form-control")))
     input_box.clear()
@@ -19,16 +19,20 @@ def load_and_play_video(driver, url, playtime, wait):
     load_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > div.container > div:nth-child(2) > div.input-group > span > button.btn.btn-primary")))
     load_button.click()
     print("Load button clicked.")
-
-    # 手动播放
-    # time.sleep(1) # 等待加载出播放按钮
-    # haohao 7.11 测试发现不加timesleep也是可以正常触发播放？还是加1s比较稳定
-    # play_button = wait.until(EC.element_to_be_clickable((By.ID, "iconPlayPause")))
-    # play_button.click()
-    # print("Play button clicked.")
-
     print("Play video for {} min.".format(playtime / 60))
-    time.sleep(playtime)
+
+    # 启动wireshark（等待offset视频稳定播放）
+    print("Wireshark starting after " + str(offset_time) + "s...")
+    time.sleep(offset_time)
+    process = subprocess.Popen(tshark_command)
+    print("Wireshark started.\n")
+    
+    # 播放视频（视频实际播放了playtime，wireshark采集了playtime-offset）
+    time.sleep(playtime - offset_time)
+
+    # 关闭wireshark
+    process.terminate()
+    print("Wireshark terminated.")
 
 def save_logs_to_file(driver, folder_name, filename):
     """获取浏览器日志并保存到文件。"""
@@ -62,15 +66,15 @@ options.add_experimental_option('excludeSwitches', ['enable-logging']) # for win
 
 ####################### 播放参数配置 #######################
 # 根据实际情况设置
-# logs_folder_name = "../../output_data/plotinus_logs/1005_logs/videos_5mbit/"    # 输出logs文件夹
-# pcaps_folder_name = "../../output_data/plotinus_pcaps/1005_pcaps/videos_5mbit/"    # 输出pcaps文件夹
-logs_folder_name = "../../output_data/test/"    # 输出logs文件夹
-pcaps_folder_name = "../../output_data/test/"    # 输出pcaps文件夹
-# server_ip = "10.18.155.4"  # 服务器IP
-server_ip = "8.146.204.75"  # 服务器IP
-bandwidth = 'xmbit'   # 服务器设置的带宽限制（需要去服务器使用tc进行限制）
+logs_folder_name = "../../output_data/plotinus_logs/1013/1013_logs/videos_300kbit/"    # 输出logs文件夹
+pcaps_folder_name = "../../output_data/plotinus_pcaps/1013/1013_pcaps/videos_300kbit/"    # 输出pcaps文件夹
+# logs_folder_name = "../../output_data/logs_test2/"    # 输出logs文件夹
+# pcaps_folder_name = "../../output_data/pcaps_test2/"    # 输出pcaps文件夹
+server_ip = "10.18.155.4"  # 服务器IP
+# server_ip = "8.146.204.75"  # 服务器IP
+bandwidth = '300kbit'   # 服务器设置的带宽限制（需要去服务器使用tc进行限制）
 # playtime = 3 * 60   # 播放时间
-playtime = 60   # 播放时间
+playtime = 120   # 播放时间
 # bandwidths = ['100kbit', '300kbit', '500kbit', '1mbit', '2mbit', '3mbit', '4mbit', '5mbit', 'xmbit']
 # TODO
 # 考虑是否可以控制远程服务器进行带宽动态切换，从而可以撰写嵌套循环实现多个带宽自动播放 TODO
@@ -78,36 +82,28 @@ playtime = 60   # 播放时间
 # 视频URL列表
 video_urls = [
     # videos1
-    # f'http://{server_ip}/BV1zi421S7QJ.mpd',
-    # f'http://{server_ip}/BV1Tx411W7eg.mpd',
-    # f'http://{server_ip}/BV1pz421a7oC.mpd',
-    # f'http://{server_ip}/BV14m421N7Q5.mpd',
-    # f'http://{server_ip}/BV12A4y1o7Gg.mpd',
-    # f'http://{server_ip}/BV1fT421v7m6.mpd',
-    # f'http://{server_ip}/BV16Y4y13724.mpd',
-    # f'http://{server_ip}/BV1Gx411y749.mpd',
-    # f'http://{server_ip}/BV1Xi4y187MR.mpd',
-    # f'http://{server_ip}/BV1P44y1F72Z.mpd',
+    # f'http://{server_ip}/BV1zi421S7QJ.mpd', # 1:12
+    # f'http://{server_ip}/BV1Tx411W7eg.mpd', # 3:24
+    # f'http://{server_ip}/BV1pz421a7oC.mpd', # 1:38
+    # f'http://{server_ip}/BV14m421N7Q5.mpd', # 1:48
+    # f'http://{server_ip}/BV12A4y1o7Gg.mpd', # 3:31
+    # f'http://{server_ip}/BV1fT421v7m6.mpd', # 3:48
+    # f'http://{server_ip}/BV16Y4y13724.mpd', # 1:39
+    # f'http://{server_ip}/BV1Gx411y749.mpd', # 4:20
+    # f'http://{server_ip}/BV1Xi4y187MR.mpd', # 2:22
+    # f'http://{server_ip}/BV1P44y1F72Z.mpd', # 9:56
 
-    # f'http://{server_ip}/BV1jJ4m1G73a.mpd',
-    # f'http://{server_ip}/BV1Am411y7iK.mpd',
-    # f'http://{server_ip}/BV1Lc411U7TT.mpd',
-    # f'http://{server_ip}/BV1Gx411y749.mpd',
-    # f'http://{server_ip}/BV1MN41187U6.mpd',
-    # f'http://{server_ip}/BV12w4m19781.mpd',
-    # f'http://{server_ip}/BV1Fu411o7o1.mpd',
-    # f'http://{server_ip}/BV1P44y1F72Z.mpd',
-    # f'http://{server_ip}/BV1Mg411S7wi.mpd',
-    # f'http://{server_ip}/BV1CM4m1k7bs.mpd',
+    f'http://{server_ip}/BV1jJ4m1G73a.mpd', # 4:26
+    f'http://{server_ip}/BV1Mg411S7wi.mpd', # 2:58
+    f'http://{server_ip}/BV1Am411y7iK.mpd', # 2:12
+    f'http://{server_ip}/BV1Lc411U7TT.mpd', # 5:51
+    f'http://{server_ip}/BV1Gx411y749.mpd', # 4:20
+    f'http://{server_ip}/BV1MN41187U6.mpd', # 2:04
+    f'http://{server_ip}/BV12w4m19781.mpd', # 5:11
+    f'http://{server_ip}/BV1Fu411o7o1.mpd', # 3:24
+    f'http://{server_ip}/BV1P44y1F72Z.mpd', # 9:56
+    f'http://{server_ip}/BV1CM4m1k7bs.mpd', # 7:01
 ]
-
-# test
-# video_urls = [
-#     # 'https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd',
-#     # 'https://dash.akamaized.net/digitalprimates/fraunhofer/480p_video/heaac_2_0_with_video/Sintel/sintel_480p_heaac2_0.mpd',
-#     f'http://{server_ip}/BV1P44y1F72Z.mpd',
-#     f'http://{server_ip}/BV1jE411k76o.mpd'
-# ]
 
 try:
     # 检查输出文件夹
@@ -138,25 +134,26 @@ try:
         pcap_file_path = os.path.join(pcaps_folder_name, pcap_file_name)
         tshark_command = [
             'E:/Wireshark/tshark.exe',  # E:\\Wireshark\\tshark.exe
-            # '-i', '以太网',  # 以太网接口名称或编号
-            '-i', 'WLAN',
+            '-i', '以太网', # 以太网口
+            # '-i', 'WLAN',   # 无线网口
             # '-w', video_number + '.pcap',
             '-w', pcap_file_path,
             '-f', f'host {server_ip}',
-            '-a', f'duration:{playtime + offset_time}'  # 捕获持续时间
+            # '-a', f'duration:{playtime + offset_time}'  # 捕获持续时间
+            '-a', f'duration:{playtime}'  # 捕获持续时间
         ]
-        print("Wireshark starting...")
-        process = subprocess.Popen(tshark_command)
-        time.sleep(offset_time)
-        print("Wireshark started.\n")
+        # print("Wireshark starting...")
+        # process = subprocess.Popen(tshark_command)
+        # time.sleep(offset_time)
+        # print("Wireshark started.\n")
 
         # 播放视频
         print("Playing video {}...".format(video_number))
-        load_and_play_video(driver, video, playtime, wait)
+        load_and_play_video(driver, video, playtime, wait, tshark_command)
         print("Playback finished.\n")
 
         # 关闭wireshark
-        process.terminate()
+        # process.terminate()
         print("Wireshark terminated.")
         print("Pcaps saved to {}.".format(pcap_file_path))
 
@@ -173,10 +170,8 @@ try:
         # time.sleep(2)
 
         # 关闭浏览器
-        # driver.quit()
-        # time.sleep(2)
-        # 暂停等待输入
-        input("Press Enter to continue...")
+        driver.quit()
+        time.sleep(2)
 
 
     # folder_name = "original_logs"
